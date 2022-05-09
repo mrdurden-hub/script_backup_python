4731d0c62c1822f8db175ba8850ac941d1febab8
Matt <cmateus297@gmail.com>
2022-05-09 16:44:31 -0300

    logging settings

--- backup.py
+++ backup.py
@@ -5,24 +5,28 @@ from pathlib import Path
 import os
 import sys
 import re
+import logging
 
-date = datetime.now()
-args = sys.argv
-backup_folder = str(f'/backup_{date.strftime("%d_%B_%Y")}')
+# primeiro arg = caminho_para ler
+# segundo arg = caminho_para_criar_backup
 
 
 def init_app():
-    # primeiro arg = caminho_para ler
-    # segundo arg = caminho_para_criar_backup
+    date = datetime.now()
+    args = sys.argv
+    backup_folder = str(f'/backup_{date.strftime("%d_%B_%Y")}')
+    logging.basicConfig(filename='example.log',
+                        level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
     if args:
         del(args[0])
         if len(args) == 0 or len(args) == 1 or len(args) > 2:
-            print('Erro: Você precisa passar 2 parâmetros')
+            logging.warning('Você precisa passar 2 parâmetros')
             return
 
         for arg in args:
             if not re.search('/', arg):
-                print('Você passou argumentos inválido')
+                logging.warning('Você passou argumentos inválido')
                 return
 
         src_path = args[0]
@@ -30,23 +34,26 @@ def init_app():
         directory = Path(backup_path)
 
     if not os.path.exists(src_path):
-        print('Erro: Caminho para backup não encontrado')
+        logging.warning('Caminho para backup não encontrado')
         return
 
     if not os.path.exists(args[1]):
-        print('Erro: Caminho de destinho não encontrado')
+        logging.warning('Caminho de destinho não encontrado')
         return
 
     if os.path.isdir(backup_path):
         rmtree(backup_path, ignore_errors=True)
 
-    copytree(src_path, backup_path,
-             ignore=ignore_patterns('node_modules', '.git', 'venv', 'env', '.env', '.venv'))
-    print('arquivos copiados')
+    try:
+        copytree(src_path, backup_path,
+                 ignore=ignore_patterns('node_modules', '.git', 'venv', 'env', '.env', '.venv'))
 
-    with ZipFile(args[1] + f'/backup_{date.strftime("%d_%B_%Y.zip")}', 'w') as zip:
-        for file in directory.rglob("*"):
-            zip.write(file, arcname=file.relative_to(directory))
+        with ZipFile(args[1] + f'/backup_{date.strftime("%d_%B_%Y.zip")}', 'w') as zip:
+            for file in directory.rglob("*"):
+                zip.write(file, arcname=file.relative_to(directory))
+            logging.info('Backup concluido')
+    except Exception as e:
+        logging.warning(e)
 
     rmtree(backup_path, ignore_errors=True)
 
