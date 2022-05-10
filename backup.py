0adc92656ddbfcb99ac40246bb30543c03125b75
Matt <cmateus297@gmail.com>
2022-05-10 10:27:56 -0300

    progress bar and click settings

--- backup.py
+++ backup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+from pydoc import cli
 from shutil import rmtree, copytree,  ignore_patterns
 from zipfile import ZipFile
 from datetime import datetime
@@ -6,27 +8,38 @@ import os
 import sys
 import re
 import logging
+import click
+from threading import Thread
+from progress.spinner import Spinner
 
-# primeiro arg = caminho_para ler
-# segundo arg = caminho_para_criar_backup
 
+@click.argument('source', required=True)
+@click.argument('dest', required=True)
+@click.command()
+def init_app(source, dest):
+    """ Use esse script para fazer backup do seu sistema linux.
+    O script recebe dois respectivos argumentos. O primeiro é o caminho do diretório escolhido para backup.
+    O segundo é o caminho do destinho do backup que será feito.
+    Ao executar o script será ignorado as pastas: 'node_modules', '.git', 'venv', 'env', '.env', '.venv"""
 
-def init_app():
+    spinner = Spinner('Realizando backup ')
     date = datetime.now()
     args = sys.argv
     backup_folder = str(f'/backup_{date.strftime("%d_%B_%Y")}')
-    logging.basicConfig(filename='example.log',
+    logging.basicConfig(filename='backup.log',
                         level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
-
     if args:
         del(args[0])
         if len(args) == 0 or len(args) == 1 or len(args) > 2:
             logging.warning('Você precisa passar 2 parâmetros')
+            click.echo(
+                'Você precisa indicar o caminho de backup e o caminho de destinho.')
             return
 
         for arg in args:
             if not re.search('/', arg):
                 logging.warning('Você passou argumentos inválido')
+                click.echo('Você passou argumentos inválido')
                 return
 
         src_path = args[0]
@@ -35,27 +48,39 @@ def init_app():
 
     if not os.path.exists(src_path):
         logging.warning('Caminho para backup não encontrado')
+        click.echo('Caminho para backup não encontrado')
         return
 
     if not os.path.exists(args[1]):
         logging.warning('Caminho de destinho não encontrado')
+        click.echo('Caminho de destinho não encontrado')
         return
 
     if os.path.isdir(backup_path):
         rmtree(backup_path, ignore_errors=True)
 
-    try:
-        copytree(src_path, backup_path,
-                 ignore=ignore_patterns('node_modules', '.git', 'venv', 'env', '.env', '.venv'))
+    def backup():
+        try:
+            copytree(src_path, backup_path,
+                     ignore=ignore_patterns('node_modules', '.git', 'venv', 'env', '.env', '.venv'))
 
-        with ZipFile(args[1] + f'/backup_{date.strftime("%d_%B_%Y.zip")}', 'w') as zip:
-            for file in directory.rglob("*"):
-                zip.write(file, arcname=file.relative_to(directory))
-            logging.info('Backup concluido')
-    except Exception as e:
-        logging.warning(e)
+            with ZipFile(args[1] + f'/backup_{date.strftime("%d_%B_%Y.zip")}', 'w') as zip:
+                for file in directory.rglob("*"):
+                    zip.write(file, arcname=file.relative_to(directory))
+                logging.info('Backup concluido')
+        except Exception as e:
+            logging.warning(e)
+
+    backup_thread = Thread(target=backup)
+    backup_thread.start()
+
+    while backup_thread.is_alive():
+
+        spinner.next()
 
     rmtree(backup_path, ignore_errors=True)
+
+    click.echo('\nBackup concluido.')
 
 
 init_app()
